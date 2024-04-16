9b291e426f11d71c973acd3dc05e34358258b236
Rayhan <rayhanshuvo.bd@gmail.com>
2024-04-16 17:49:03 +0600

    added xml
    added xml

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "address")
+    private String address;
+
+}
